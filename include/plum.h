e5793145312b38e35329215e6d65bbc98c212a8e
Yunoshin Tani <taniyunoshin@gmail.com>
2026-04-19 20:19:43 +0900

    fix: set main header to umbrella header for clangd

--- include/plum.h
+++ include/plum.h
@@ -1,25 +1,25 @@
 #pragma once
 
 // Definitions
-#include "interfaces/pin_names.h"  // IWYU pragma: keep
+#include "interfaces/pin_names.h"  // IWYU pragma: export
 
 // Drivers
-#include "interfaces/can.h"          // IWYU pragma: keep
-#include "interfaces/digital_in.h"   // IWYU pragma: keep
-#include "interfaces/digital_out.h"  // IWYU pragma: keep
-#include "interfaces/pwm_out.h"      // IWYU pragma: keep
-#include "interfaces/sleep.h"        // IWYU pragma: keep
-#include "interfaces/thread.h"       // IWYU pragma: keep
-#include "interfaces/time.h"         // IWYU pragma: keep
+#include "interfaces/can.h"          // IWYU pragma: export
+#include "interfaces/digital_in.h"   // IWYU pragma: export
+#include "interfaces/digital_out.h"  // IWYU pragma: export
+#include "interfaces/pwm_out.h"      // IWYU pragma: export
+#include "interfaces/sleep.h"        // IWYU pragma: export
+#include "interfaces/thread.h"       // IWYU pragma: export
+#include "interfaces/time.h"         // IWYU pragma: export
 
 #ifdef __MBED_CONFIG_DATA__
-#include "platforms/mbed/can.h"          // IWYU pragma: keep
-#include "platforms/mbed/digital_in.h"   // IWYU pragma: keep
-#include "platforms/mbed/digital_out.h"  // IWYU pragma: keep
-#include "platforms/mbed/pwm_out.h"      // IWYU pragma: keep
-#include "platforms/mbed/sleep.h"        // IWYU pragma: keep
-#include "platforms/mbed/thread.h"       // IWYU pragma: keep
-#include "platforms/mbed/time.h"         // IWYU pragma: keep
+#include "platforms/mbed/can.h"          // IWYU pragma: export
+#include "platforms/mbed/digital_in.h"   // IWYU pragma: export
+#include "platforms/mbed/digital_out.h"  // IWYU pragma: export
+#include "platforms/mbed/pwm_out.h"      // IWYU pragma: export
+#include "platforms/mbed/sleep.h"        // IWYU pragma: export
+#include "platforms/mbed/thread.h"       // IWYU pragma: export
+#include "platforms/mbed/time.h"         // IWYU pragma: export
 
 using namespace plum::platform::mbed;
 #endif
